b265c1fe26ce7b75645a8c2c9f34c936719d77f9
Joe Sun <joe.sun@berkeley.edu>
2017-11-30 02:19:32 -0800

    Current address

---
+++ Router/Router/LocationListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LocationListTableViewCell.swift
+//  Router
+//
+//  Created by Joe Sun on 11/30/17.
+//  Copyright © 2017 JoeSun. All rights reserved.
+//
+
+import UIKit
+
+class LocationListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var searchBar: UITextField!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Router/Router/View Controllers/LocationListViewController.swift
@@ -0,0 +1,101 @@
+//
+//  LocationListViewController.swift
+//  Router
+//
+//  Created by Joe Sun on 11/30/17.
+//  Copyright © 2017 JoeSun. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class LocationListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var sourceField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    let locationManager = CLLocationManager()
+    var locationTuples: [(textField: UITextField?, mapItem: MKMapItem?)]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.requestLocation()
+        }
+        
+        locationTuples = [(sourceField, nil)]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! LocationListTableViewCell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func formatAddressFromPlacemark(placemark: CLPlacemark) -> String {
+        return (placemark.addressDictionary!["FormattedAddressLines"] as!
+            [String]).joined(separator: ", ")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
+extension LocationListViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(locations.last!, completionHandler:
+            {(placemarks: Optional<Array<CLPlacemark>>, error: Optional<Error>) -> () in
+                if let placemarks = placemarks {
+                    let placemark = placemarks[0]
+                    self.locationTuples[0].mapItem = MKMapItem(placemark:
+                        MKPlacemark(coordinate: placemark.location!.coordinate,
+                                    addressDictionary: placemark.addressDictionary as! [String:AnyObject]?))
+                    self.sourceField.text = self.formatAddressFromPlacemark(placemark: placemark)
+                }
+        })
+        
+    }
+    
+}
